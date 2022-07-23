398d464cd473c178afc99943406e373ca04e8491
Geurin <stumbletrip88@gmail.com>
2022-07-24 00:07:03 +0900

    loading image import
    geurin

---
+++ app/src/main/java/com/example/ardelproject/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.ardelproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceStare) {
+        super.onCreate(savedInstanceStare);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
