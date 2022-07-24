8e139497e8b930ba4998b297aca66758669cb502
Geurin <stumbletrip88@gmail.com>
2022-07-24 12:46:59 +0900

    greadle setting
    geurin

--- app/src/main/java/com/example/ardelproject/Scan.java
+++ app/src/main/java/com/example/ardelproject/Scan.java
@@ -1,5 +1,6 @@
 package com.example.ardelproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.unity3d.player.UnityPlayerActivity;
+
 public class Scan extends Fragment {
 
     private View view;
@@ -17,6 +20,9 @@ public class Scan extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.scan,container,false);
+
+        Intent intent = new Intent(getActivity(), UnityPlayerActivity.class);
+        startActivity(intent);
 
         return view;
     }
