48ff5afce62e06b9087e3a4a079c2e1146a7ea9f
Geurin <stumbletrip88@gmail.com>
2022-07-27 03:02:51 +0900

    card view modify
    - 카드뷰 이미지 수정

--- app/src/main/java/com/example/ardelproject/Settings.java
+++ app/src/main/java/com/example/ardelproject/Settings.java
@@ -1,9 +1,11 @@
 package com.example.ardelproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +14,22 @@ import androidx.fragment.app.Fragment;
 public class Settings extends Fragment {
 
     private View view;
+    private Button logoutBtn;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.settings,container,false);
+
+        logoutBtn = (Button) view.findViewById(R.id.btn_logout);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), Login.class);
+                startActivity(intent); // 다음화면으로 넘어가기
+//                finish(); // Activity 화면 제거
+            }
+        });
 
         return view;
     }
