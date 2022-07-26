436bc6e44295a157f62f0d1862bb27c2f98934c6
Geurin <stumbletrip88@gmail.com>
2022-07-26 23:38:07 +0900

    cardview
    - 카드뷰 구현
    - framelayout, navigation bar 정상화

--- app/src/main/java/com/example/ardelproject/Home.java
+++ app/src/main/java/com/example/ardelproject/Home.java
@@ -8,10 +8,16 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends Fragment {
 
     private View view;
+    private RecyclerView recyclerview;
 
     @Nullable
     @Override
