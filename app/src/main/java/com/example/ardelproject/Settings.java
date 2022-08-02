5368438c34eb788700bb04c91b284d00eab9846c
Geurin <stumbletrip88@gmail.com>
2022-08-02 18:00:58 +0900

    add set2 page
    - set2 페이지 추가
    - 세부 디자인 조금씩 수정

--- app/src/main/java/com/example/ardelproject/Settings.java
+++ app/src/main/java/com/example/ardelproject/Settings.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,11 +16,21 @@ public class Settings extends Fragment {
 
     private View view;
     private Button logoutBtn;
+    private ImageButton infoset;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.settings,container,false);
+
+        infoset = (ImageButton) view.findViewById(R.id.btn_info_setting);
+        infoset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), setting02.class);
+                startActivity(intent); // 다음화면으로 넘어가기
+            }
+        });
 
         logoutBtn = (Button) view.findViewById(R.id.btn_logout);
         logoutBtn.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/ardelproject/setting02.java
@@ -0,0 +1,14 @@
+package com.example.ardelproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setting02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting02);
+    }
+}
