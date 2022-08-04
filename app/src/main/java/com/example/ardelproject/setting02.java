287200ab2cb28650d685010ea048e2cd1a250bf7
Geurin <stumbletrip88@gmail.com>
2022-08-04 16:38:43 +0900

    commit

--- app/src/main/java/com/example/ardelproject/setting02.java
+++ app/src/main/java/com/example/ardelproject/setting02.java
@@ -9,18 +9,14 @@ import android.widget.CompoundButton;
 
 public class setting02 extends AppCompatActivity {
 
-    SwitchCompat sw1;
-    SwitchCompat sw2;
-    SwitchCompat sw3;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting02);
 
-        sw1 = (SwitchCompat) findViewById(R.id.btn_sw1);
-        sw2 = (SwitchCompat) findViewById(R.id.btn_sw2);
-        sw3 = (SwitchCompat) findViewById(R.id.btn_sw3);
+        SwitchCompat sw1 = (SwitchCompat) findViewById(R.id.btn_sw1);
+        SwitchCompat sw2 = (SwitchCompat) findViewById(R.id.btn_sw2);
+        SwitchCompat sw3 = (SwitchCompat) findViewById(R.id.btn_sw3);
 
         sw1.setChecked(true);
         sw2.setChecked(true);
