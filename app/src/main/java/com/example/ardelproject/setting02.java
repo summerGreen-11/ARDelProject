8180e24a9e6d6beb0dc1053b37b904842f2df070
Geurin <stumbletrip88@gmail.com>
2022-08-04 16:38:06 +0900

    Add switch button

--- app/src/main/java/com/example/ardelproject/setting02.java
+++ app/src/main/java/com/example/ardelproject/setting02.java
@@ -1,14 +1,29 @@
 package com.example.ardelproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
 
 public class setting02 extends AppCompatActivity {
+
+    SwitchCompat sw1;
+    SwitchCompat sw2;
+    SwitchCompat sw3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting02);
+
+        sw1 = (SwitchCompat) findViewById(R.id.btn_sw1);
+        sw2 = (SwitchCompat) findViewById(R.id.btn_sw2);
+        sw3 = (SwitchCompat) findViewById(R.id.btn_sw3);
+
+        sw1.setChecked(true);
+        sw2.setChecked(true);
+        sw3.setChecked(true);
     }
 }
