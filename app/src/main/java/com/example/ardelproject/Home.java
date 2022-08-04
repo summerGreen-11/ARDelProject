48cd8c472884f167fa88b17b00365b19ffbbe560
goyeongkun <ghlv0510@gmail.com>
2022-08-04 18:45:29 +0900

    commit test

--- app/src/main/java/com/example/ardelproject/Home.java
+++ app/src/main/java/com/example/ardelproject/Home.java
@@ -48,6 +48,7 @@ public class Home extends Fragment {
             }
         });
 
+        btning.setSelected(true);
         btning.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
